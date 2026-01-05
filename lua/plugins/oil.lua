74ac60fc1fa5c7b4fe1c073cc77b1f58b8395957
Paul K. <paulkchem@gmail.com>
2026-01-05 09:17:35 +0100

    phase 3.2: add oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
+  keys = {
+    { "<leader>e", "<cmd>Oil<CR>", desc = "Open file [E]xplorer (Oil)" },
+  },
+}
